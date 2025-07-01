10b125944acb627a2652553fd3317f30d1e27460
Peter Xia <aattyy@gmail.com>
2025-07-01 10:34:59 -0700

    update

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -7,7 +7,8 @@ while [ true ]; do
 		--plugin-dir "/home/.local/share/streamlink/plugins" \
 		--output /home/download/"{time:%Y%m%d%H%M%S}-{id}-{title}".mkv \
 		--hls-live-restart \
-		"--twitch-api-header=Authorization=OAuth ${TWITCH_TOKEN}"
-		"$STREAMLINK_OPTS" $STREAM_URL $QUALITY
+		--twitch-api-header="Authorization=OAuth ${TWITCH_TOKEN}" \
+		--twitch-supported-codecs=h264,h265,av1 \
+		$STREAM_URL $QUALITY
 	sleep ${RETRY_SECS}s
 done

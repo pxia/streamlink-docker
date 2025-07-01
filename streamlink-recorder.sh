9676cf8fd05888f3df27af20b8b333a62cf68284
Peter Xia <aattyy@gmail.com>
2025-07-01 10:27:47 -0700

    add token

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -7,6 +7,7 @@ while [ true ]; do
 		--plugin-dir "/home/.local/share/streamlink/plugins" \
 		--output /home/download/"{time:%Y%m%d%H%M%S}-{id}-{title}".mkv \
 		--hls-live-restart \
+		"--twitch-api-header=Authorization=OAuth ${TWITCH_TOKEN}"
 		"$STREAMLINK_OPTS" $STREAM_URL $QUALITY
 	sleep ${RETRY_SECS}s
 done

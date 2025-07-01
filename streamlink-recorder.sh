08cc1c06ec44450e168ad15cd325d76e27028efd
Peter Xia <aattyy@gmail.com>
2025-07-01 10:40:24 -0700

    update

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -3,9 +3,8 @@
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
 while [ true ]; do
-	streamlink \
-		--plugin-dir "/home/.local/share/streamlink/plugins" \
-		--output /home/download/"{time:%Y%m%d%H%M%S}-{id}-{title}".mkv \
+	echostreamlink \
+		--output=/home/download/"{time:%Y%m%d%H%M%S}-{id}-{title}".mkv \
 		--hls-live-restart \
 		--twitch-api-header="Authorization=OAuth ${TWITCH_TOKEN}" \
 		--twitch-supported-codecs=h264,h265,av1 \
